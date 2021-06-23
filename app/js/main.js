f47a9ed54f3fd21bc0312bd4afac2b336abe2bcc
mars444 <petromars444@gmail.com>
2021-06-23 15:16:16 +0300

    added slider

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+    $('.zay__slider-inner').slick({
+      nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"></button>' ,
+      prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"></button>' ,
+      dots:true,
+    
+      });
+
+
+
+});
