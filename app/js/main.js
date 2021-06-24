2932cbd0819e6b2072812ae8b007d6f8720deaa8
mars444 <petromars444@gmail.com>
2021-06-24 17:35:11 +0300

    added page shop.html

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,28 @@ $(function(){
     
       });
 
+   
+      var acc = document.getElementsByClassName("categories-type-link");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("categories-type-link-active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+$('.brands__slider').slick({
+   nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"></button>' ,
+   prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"></button>' ,
+   slidesToShow:4,
+ 
+   });
 
 });
